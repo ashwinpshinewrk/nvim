34fc8ea71526308cd1596dc17135ea4bb2cfbbdf
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-06-27 21:17:53 +0530

    Removed FileSystem and Icon support for it

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---Set Leader "<leader>"
+
 vim.g.mapleader = " "
 vim.g.localmapleader = " "
 
@@ -6,7 +6,6 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
 require("config.lazy")
 
 vim.cmd[[colorscheme tokyonight-night]]
