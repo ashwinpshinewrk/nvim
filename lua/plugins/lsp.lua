1bb0de1fee36af4c886e425fea837973fbd6419f
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-10-23 21:16:36 +0530

    Added gopls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,59 +1,55 @@
 -- lsp.lua
 return {
-  "neovim/nvim-vim.lsp.config",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-vim.lsp.config.nvim",
-    "hrsh7th/cmp-nvim-lsp", -- needed for autocomplete
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-vim.lsp.config").setup({
-      ensure_installed = { "lua_ls", "ts_ls", "pyright", "zls", "rust_analyzer", "clangd", "svelte", "tailwindcss"}, -- Add more if needed
-      automatic_installation = true,
-    })
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig",
+        "hrsh7th/cmp-nvim-lsp", -- needed for autocomplete
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "ts_ls", "pyright", "zls", "rust_analyzer", "clangd", "svelte", "tailwindcss", "gopls" }, -- Add more if needed
+            automatic_installation = true,
+        })
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    vim.diagnostic.config({
-      virtual_text = false,
-      signs = true,
-      underline = true,
-      update_in_insert = false,
-      severity_sort = true,
-      float = {
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        vim.diagnostic.config({
+            virtual_text = false,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
 
-    vim.o.updatetime = 250
-    vim.cmd([[
+        vim.o.updatetime = 250
+        vim.cmd([[
       autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focus = false })
     ]])
 
+        vim.lsp.config('*', {
+            capabilities = capabilities,
+        })
 
+        -- Custom targets
+        vim.lsp.config('svelte', {
 
-    local servers = { "lua_ls", "ts_ls", "pyright", "zls","rust_analyzer","clangd",  }
+            capabilities = capabilities,
+            filetypes = { "svelte" },
+            root_dir = require("vim.lsp.config").util.root_pattern("package.json", "svelte.config.js", ".git")
+        })
 
-    for _, server in ipairs(servers) do
-      vim.lsp.config[server].setup({
-        capabilities = capabilities,
-      })
-    end
-vim.lsp.config.svelte.setup({
-
-        capabilities = capabilities,
-        filetypes = {"svelte"},
-        root_dir = require("vim.lsp.config").util.root_pattern("package.json","svelte.config.js", ".git")
-    })
-
-    vim.lsp.config.tailwindcss.setup({
-    capabilities = capabilities,
-        filetypes = {"html","css","javascript", "typescript", "svelte"},
-        root_dir = require("vim.lsp.config").util.root_pattern("tailwind.config.js", "package.json")
-    })
+        vim.lsp.config('tailwindcss', {
+            capabilities = capabilities,
+            filetypes = { "html", "css", "javascript", "typescript", "svelte" },
+            root_dir = require("vim.lsp.config").util.root_pattern("tailwind.config.js", "package.json")
+        })
     end
 }
